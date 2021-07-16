0c3bce7f12e7d2526f2cc2bcc5265e7e0c67ccd4
Luis Ribeiro <56344898+luis1ribeiro@users.noreply.github.com>
2021-07-16 14:22:51 +0100

    Masters

---
+++ PLC/PRC/PRC2021-Projeto/App/api-server/controllers/bars.js
@@ -0,0 +1,65 @@
+var Bars = require("../models/bars");
+
+
+
+module.exports.listBars = () => {
+	return Bars.find().exec();
+};
+
+module.exports.lookUp = (id) => {
+	return Bars.findOne({
+		_id: id,
+	}).exec();
+};
+
+module.exports.deleteBar = (id) => {
+    return Bars.deleteOne({
+        _id: id
+    });
+};
+
+module.exports.insertBar = (b) => {
+    var newC = new Bars(b);
+    return newC.save();
+};
+
+// add visit by user
+module.exports.addvisit = (id, idU) => {
+	return Bars.updateOne(
+		{
+			_id: id,
+		},
+		{
+			$push: {
+				"visits.authors": idU,
+			},
+			$inc: {
+				"visits.count": 1,
+			},
+		}
+	).exec();
+};
+
+// remove visit by user
+module.exports.removevisit = (id, idU) => {
+	return Bars.updateOne(
+		{
+			_id: id,
+		},
+		{
+			$pull: {
+				"visits.authors": idU,
+			},
+			$inc: {
+				"visits.count": -1,
+			},
+		}
+	).exec();
+};
+
+// get bars visited by user
+module.exports.getvisits = (idU) => {
+    return Bars.find({
+        "visits.authors": idU
+    });
+};
---
+++ PLC/PRC/PRC2021-Projeto/App/api-server/controllers/locations.js
@@ -0,0 +1,64 @@
+var Locations = require("../models/locations");
+
+module.exports.listLocations = () => {
+	return Locations.find().exec();
+};
+
+module.exports.lookUp = (id) => {
+	return Locations.findOne({
+		_id: id,
+	}).exec();
+};
+
+module.exports.deleteLocation = (id) => {
+    return Locations.deleteOne({
+        _id: id
+    });
+};
+
+module.exports.insertLocation = (l) => {
+    var newC = new Locations(l);
+    return newC.save();
+};
+
+
+// add visit by user
+module.exports.addvisit = (id, idU) => {
+	return Locations.updateOne(
+		{
+			_id: id,
+		},
+		{
+			$push: {
+				"visits.authors": idU,
+			},
+			$inc: {
+				"visits.count": 1,
+			},
+		}
+	).exec();
+};
+
+// remove visit by user
+module.exports.removevisit = (id, idU) => {
+	return Locations.updateOne(
+		{
+			_id: id,
+		},
+		{
+			$pull: {
+				"visits.authors": idU,
+			},
+			$inc: {
+				"visits.count": -1,
+			},
+		}
+	).exec();
+};
+
+// get locations visited by user
+module.exports.getvisits = (idU) => {
+    return Locations.find({
+        "visits.authors": idU
+    });
+};
